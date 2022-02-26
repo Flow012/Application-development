3325698240c204a777db3e99c0d842d53ca4cf1b
HarrelFuntilon <funtilonh023@gmail.com>
2022-02-26 12:27:06 +0800

    restored main dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-<<<<<<< Updated upstream
 class MyApp extends StatelessWidget {
-=======
-class Mpp extends StatelessWidget {
->>>>>>> Stashed changes
   const MyApp({Key? key}) : super(key: key);
 
   @override
